0c66ad9ed704792dc62ff9e289fce19cf1967bb9
Steve Zelaznik <zelaznik@users.noreply.github.com>
2015-01-26 10:41:04 -0500

    Update frozen_dict.py
    Made the resolving of hash collisions more robust.  When I realized that sets to not iterate in a consistent order, I abandoned the sorting method.  Items are still sorted by hash value, and hash collisions are still rare.  About 1 collision for every 1400 items.  When there is a hash collision, the corresponding items are put into a subclass of frozenset.

--- frozen_dict.py
+++ frozen_dict.py
@@ -5,22 +5,76 @@ if 3 / 2 == 1:
 elif 3 / 2 == 1.5:
     __version__ = 3
     imap = map
-    
-def on_error_plain(func):
-    ''' Prevents a problem with the object's "repr" function from 
-        bringing everything to a screeching halt.'''
-    def __repr__(self):
-        try:
-            return func(self)
-        except Exception:
-            return object.__repr__(self)
-    return __repr__
-    
+    from functools import reduce
+
+import itertools as it
 from operator import itemgetter, methodcaller, attrgetter
-from bisect import bisect_left, bisect_right
+from bisect import bisect_left
+
+from_iterable = it.chain.from_iterable
+
+flipped = itemgetter(1,0)
+pop_first = methodcaller('pop', 0)
+pop_last = methodcaller('pop', -1)
 hash_caller = methodcaller('__hash__')
 
-def tuple_getter(i):
+key_getter = attrgetter('key')
+val_getter = attrgetter('value')
+pair_getter = attrgetter('pair')
+
+class Item(tuple):
+    key   = property(itemgetter(0))
+    value = property(itemgetter(1))
+    pair  = property(itemgetter(0,1))
+    __slots__ = ()
+    def __hash__(self):
+        return hash(self.key)
+        
+f_iter = frozenset.__iter__
+class Group(frozenset):
+    ''' Container to store multiple items during a hash collision.'''
+    __slots__ = ()
+    def __new__(cls, items):
+        wrapped = map(Item, items)
+        return frozenset.__new__(cls, wrapped)
+    def __iter__(self):
+        return self.items()
+    def items(self):
+        return map(pair_getter, f_iter(self))
+    def __hash__(self):
+        frz = frozenset(self.items())
+        return hash(frz)
+    def __repr__(self):
+        return frozenset.__repr__(self)
+
+t_iter = tuple.__iter__
+class Single(tuple):
+    ''' The singleton counterpart to "Group".  This is a regular tuple,
+        except the iteration is overloaded so that (key, value)
+        actually iterates as though it were ((key, value),)
+        
+        This is done because the method FrozenDict.__getitem__
+        loops through either a Single or Group looking for a
+        matching key.
+        
+        I could store single items inside single-element frozensets
+        but that would be a huge waste of space and resources.
+       
+        In testing 5 million hashes I only found collisions in 1/1400 items.
+    '''
+      
+    key   = property(itemgetter(0))
+    value = property(itemgetter(1))
+    pair  = property(itemgetter(0,1))
+    __slots__ = ()
+    def __iter__(self):
+        return t_iter((self.pair,))
+    def __len__(self):
+        return 1
+    def __repr__(self):
+        return 'Single(%r, %r)' % self.pair
+
+def col(i):
     # We can't use itemgetter when we've overloaded
     # __getitem__ so we need to bind the method
     # directly from tuple.
@@ -28,18 +82,6 @@ def tuple_getter(i):
     def _col(self):
         return g(self,i)
     return _col
-
-hash_getter = tuple_getter(0)
-pair_getter = tuple_getter(1)
-
-pop_first = methodcaller('pop', 0)
-pop_last = methodcaller('pop', -1)
-col_0 = itemgetter(0)
-col_1 = itemgetter(1)
-col_2 = itemgetter(2)
-col_3 = itemgetter(3)
-cols_1_2 = itemgetter(1,2)
-flipped = itemgetter(1,0)
 
 doc_str = '''\
 Behaves in most ways like a regular dictionary, except that it's immutable.
@@ -75,204 +117,177 @@ FrozenDict supports bi-directional conversions with regular dictionaries.
     [out] >>> FrozenDict({'x': 3, 'y': 4, 'z': 5})
     [in]  >>> dict(FrozenDict(original))
     [out] >>> {'x': 3, 'y': 4, 'z': 5}   '''
-    
-class Item(tuple):
-    ''' A special container to make all items hashable.
-        We put all the dict entries into a set and 
-        record the order in which they are iteratively
-        returned. The items are eventually stored inside
-        a tuple, but this ensures that everything is sorted
-        consistently before calculating the hash of 
-        the entire dictionary.'''
-        
-    h = property(col_0)
-    key = property(col_1)
-    value = property(col_2)
-    pair = property(cols_1_2)
 
-    def __new__(cls, pair):
-        key, value = pair
-        h = hash(key)
-        t = (h, key, value)
-        return tuple.__new__(cls, t)
-
-    def __hash__(self):
-        return self[0]
-        
-    def __cmp__(self, other):
-        try:
-            d_hash = self[0] - hash(other)
-            if d_hash < 0:
-                return -1
-            if d_hash > 0:
-                return 1
-            
-            #Now we're into hash collisions
-            self_key = self[1]
-            other_key = other.key
-            if self_key < other_key:
-                return -1
-            if self_key > other_key:
-                return 1
-                
-            # Before checking whether the 
-            # the two keys, stick it into a set
-            #and see which one is returned first
-            item = {self_key, other_key}.pop()
-            if item is self_key:
-                return -1
-            else:
-                return 1
-
-        except Exception:
-            return None
-
-    def __eq__(self, other):
-         comp = self.__cmp__(other)
-         return comp == 0
-    
-    def __lt__(self, other):
-         comp = self.__cmp__(other)
-         return comp == -1
-
-    def __gt__(self, other):
-        comp = self.__cmp__(other)
-        return comp == 1
-
-    @on_error_plain
-    def __repr__(self):
-        return 'Item((%r, %r))' % (self[1], self[2])
-        
 class FrozenDict(tuple):
     __doc__ = doc_str
-
-    tuple_getter = tuple.__getitem__
-    _hashes = property(hash_getter)
-    _pairs = property(pair_getter)
     __slots__ = ()
-    
-    def __new__(cls, orig={}, **kw):
-        #Sort the items by the hashes of their keys
-        #The parent tuple has two items.
-        #The first is a 
-        if kw:
-            if orig:
-                d = dict(orig, **kw)
-            else:
-                d = kw
-            orig, kw = None, None
-            pairs = map(Item, d.items())
-            del d
+    _hashes = property(col(0))
+    _groups  = property(col(1))
 
+    @staticmethod
+    def item_adder(dct, item):
+        ''' This is an agg function which groups items by the hash of the key.'''
+        h = hash(item[0])
+        if h not in dct:
+            dct[h] = [item]
         else:
+            dct[h].append(item)
+        return dct
+
+    @staticmethod
+    def grouper(dct, h):
+        ''' Unpacks the items for each hash value.  The groups that have 
+            only one item are stored in a subclass of tuple.
+            The ones with multiple items are put into a subclass of frozenset.'''
+        grp = dct[h]
+        if len(grp) == 1:
+            ''' Most groups contain only a single item
+                So we simply unpack it from its original
+                container which speeds up lookup time.'''
+            grp = Single(grp[0])
+        else:
+            ''' In the .01% of cases where a hash collision has occurred
+                The items are stored inside a subclass of frozenset.'''
+            grp = Group(grp)
+        dct[h] = grp
+        return dct
+
+    def __new__(cls, orig = {}, **kw):
+        if orig and kw:
+            raise ValueError('You can only pass an iterable, or keyword arguments, not both.')
+        elif orig and not kw:
             try:
-                pairs = map(Item, orig.items())
+                items = orig.items()
             except AttributeError:
-                pairs = map(Item, orig)
+                items = orig
+        elif kw and not orig:
+            items = kw.items()
+        else:
+            items = []
 
-        pairs = sorted(pairs)
-        hashes = tuple(map(col_0, pairs))
-        pairs = tuple(map(attrgetter('pair'), pairs))
-
-        t = (hashes, pairs)
-        return tuple.__new__(cls, t)
+        dct = reduce(cls.item_adder, items, {})
+        length = sum(map(len, dct.values()))
+        dct = reduce(cls.grouper, tuple(dct), dct)
+        dct = list(map(list, dct.items()))
+        dct.sort(key=itemgetter(0))
         
-    def __hash__(self):
-        return hash(self._pairs)
+        hashes = tuple(map(pop_first, dct))
+        items = tuple(map(pop_first, dct))
+        t = (hashes,items,length)
+        return tuple.__new__(cls,t)
 
+    def hashes(self):
+        return tuple.__iter__(self._hashes)
+
+    def groups(self):
+        return tuple.__iter__(self._groups)
+
+    def items(self):
+        return from_iterable(self.groups())
+
+    def keys(self):
+        return map(itemgetter(0), self.items())
+        
+    def values(self):
+        return map(itemgetter(1), self.items())
+
+    def __iter__(self):
+        return map(itemgetter(0), self.items())
+        
+    def __getitem__(self, key):
+        if not self:
+            raise KeyError(key)
+
+        x = hash(key)
+        lkp = tuple.__getitem__(self, 0)
+
+        if (x < lkp[0]) or (x > lkp[-1]):
+            raise KeyError(key)
+
+        idx = bisect_left(lkp, x)
+        if lkp[idx] != x:
+            raise KeyError(key)
+
+        grp = tuple.__getitem__(self, 1)[idx]
+        for k0, v0 in grp:
+            if k0 != key:
+                continue
+            return v0
+
+        raise KeyError(key)
+
+    def __contains__(self, key):
+        try:
+            self[key]
+            return True
+        except KeyError:
+            return False
+
+    def get(self, key, d = None):
+        try:
+            return self[key]
+        except KeyError:
+            return d
+    
+    def __repr__(self):
+        cls = self.__class__.__name__
+        f = lambda i: '%r : %r' % i
+        _repr = ', '.join(map(f, self.items()))
+        return '%s({%s})' % (cls, _repr)
+        
     def __eq__(self, other):
         if not isinstance(other, FrozenDict):
             other = FrozenDict(other)
         return tuple.__eq__(self, other)
-
-    def __bisecter__(self, key, in_only=False, on_error_None=False):
-        x = hash(key)
-        lkp = tuple.__getitem__(self, 0) # self._hashes
-        if (not self) or x < lkp[0] or x > lkp[-1]:
-            if in_only:
-                return False
-            if on_error_None:
-                return None
-            else:
-                raise KeyError(key)
-
-        lo = bisect_left(lkp, x)
-        hi = bisect_right(lkp, x, lo)
-        if hi > lo:
-            slc = lkp[lo:hi]
-            assert set(slc) == {x}
-            
-            items = tuple.__getitem__(self, 1)[lo:hi]
-            for k0,v0 in items:
-                if k0 != key:
-                    continue
-                if in_only:
-                    return True
-                else:
-                    return v0
-
-        if in_only:
-            return False
-        if on_error_None:
-            return None
-        else:
-            raise KeyError(key)
-
-    def __getitem__(self, key):
-        return self.__bisecter__(key)
         
-    def __contains__(self, key):
-        return self.__bisecter__(key, in_only=True)
-
-    def get(self, key):
-        return self.__bisecter__(key,on_error_None=True)
-
-    @on_error_plain
-    def __repr__(self):
-        fcn = lambda pair: '%r: %r' % pair
-        cls_name = self.__class__.__name__
-        _repr = ', '.join(map(fcn, self.items()))
-        final = '%s({%s})' % (cls_name, _repr)
-        return final
+    def __hash__(self):
+        return hash(self._groups)
         
     def __len__(self):
-        return len(hash_getter(self))
+        return tuple.__getitem__(self, 2)
 
-    def __iter__(self):
-        return imap(col_0, self.items())
-        
-    def keys(self):
-        return map(col_0, self.items())
-
-    def values(self):
-        return map(col_1, self.items())
-        
-    def items(self):
-        itr = tuple.__iter__
-        return itr(pair_getter(self))
-        
     @classmethod        
     def fromkeys(cls, keys, value):
         return cls(dict.fromkeys(keys, value))
 
 if __version__ == 2:
-    class Python2(FrozenDict):
+    iterkey_getter = col(0)
+    itervalue_getter = col(1)
+    iteritem_getter = col(slice(None,2))
+
+    class Python2(tuple):
         __doc__ = doc_str
         tuple_getter = tuple.__getitem__
         
-        def items(self):
-            return list(pair_getter(self))
+        def __iter__(self):
+            return imap(itemgetter(0), self.items())
 
-        def iterkeys(self):
-            items = imap(pair_getter, self)
-            return imap(col_0, items)
-
-        def itervalues(self):
-            items = imap(pair_getter, self)
-            return imap(col_1, items)
-
+        def iterhashes(self):
+            return tuple.__iter__(self._hashes)
+    
+        def itergroups(self):
+            return tuple.__iter__(self._groups)
+    
         def iteritems(self):
-            return imap(pair_getter, self)
+            return from_iterable(self.itergroups())
+    
+        def iterkeys(self):
+            return imap(itemgetter(0), self.iteritems())
+            
+        def itervalues(self):
+            return imap(itemgetter(1), self.iteritems())
+
+        def hashes(self):
+            return list(self.iterhashes())
+            
+        def keys(self):
+            return list(self.iterkeys())
+            
+        def values(self):
+            return list(self.itervalues())
+            
+        def items(self):
+            return list(self.iteritems())
 
         def has_key(self, key):
             return key in self
@@ -288,7 +303,3 @@ if __version__ == 2:
     py2.update({k: dct[k] for k in dct})
     
     FrozenDict = type('FrozenDict', (tuple,), py2)
-
-if __name__ == '__main__':
-    orig = dict({Item(('x',3)): 4, Item(('x',4)): 4}, a = 1, b= 2 ,c = 3)
- 
