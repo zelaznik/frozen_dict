eee3e0dc1c7c0bf31bf7d6a6c8afe299ceb300ff
Steve Zelaznik <zelaznik@users.noreply.github.com>
2015-06-09 13:38:37 -0400

    Create freeze_recursive.py
    If FrozenDict(d) is like a shallow copy, freeze(d) is like a deep copy.  It recursively converts all the objects into something that's hashable, at least it tries to.  Certain objects such as slices will never be able to be hashable.

---
+++ freeze_recursive.py
@@ -0,0 +1,43 @@
+from frozen_dict import FrozenDict
+
+def freeze(obj):
+    ''' Recursive function which turns dictionaries into 
+        FrozenDict objects, lists into tuples, and sets
+        into frozensets.
+
+        THIS FUNCTION IS IN BETA AND HAS NOT BEEN
+        EXTENSIVELY TESTED.
+    '''
+
+    try:
+        #See if the object is hashable
+        hash(obj)
+        return obj
+    except TypeError:
+        pass
+
+    try:
+        #Try to see if this is a mapping
+        try:
+            obj[tuple(obj)]
+        except KeyError:
+            pass
+        is_mapping = True
+    except TypeError:
+        is_mapping = False
+        
+    if is_mapping:
+        frz = {k: freeze(obj[k]) for k in obj}
+        return FrozenDict(frz)
+
+    # See if the object is a set like
+    # or sequence like object
+    try:
+        obj[0]
+        cls = tuple
+    except TypeError:
+        cls = frozenset
+    except IndexError:
+        cls = tuple
+
+    return cls(freeze(i) for i in obj)
