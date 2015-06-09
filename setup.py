d5ce77088aa4edb38253caf14f5b7438be74d382
Steve Zelaznik <zelaznik@users.noreply.github.com>
2015-06-09 12:26:57 -0400

    Update setup.py
    removed the cython requirement.  Allowed the auto-generated .c files to be installed directly.

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(
-    name = "frozen_dict",
-    ext_modules = cythonize("frozen_dict.pyx")
-)
+from distutils.core import setup
+from distutils.extension import Extension
+
+if 3/2 == 1.5:
+    version = 3
+else:
+    version = 2
+
+if version == 3:
+    setup(
+        ext_modules = [Extension('frozen_dict', ['frozen_dict_py3.c'])]
+    )
+else:
+    setup(
+        ext_modules = [Extension('frozen_dict', ['frozen_dict_py2.c'])]
+    )
